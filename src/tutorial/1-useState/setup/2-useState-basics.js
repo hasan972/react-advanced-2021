27e14fd54fe1ff7345a38f2b2b8d485bfe0342a6
Shanto <rakibulhasansanto25@gmail.com>
2021-04-11 23:16:48 +0600

    useState-Basic

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,18 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+ const [text,setText] = useState('hellow world');
+ const handelClick = () =>{
+   if(text==='hellow world 1'){
+     setText('hellow world 2')
+   }else{
+     setText('hellow world 1')
+   }
+ };
+  return (<React.Fragment>
+    <h1>{text}</h1>
+    <button className="btn" onClick={handelClick}>change the value</button>
+  </React.Fragment>)
 };
 
 export default UseStateBasics;
