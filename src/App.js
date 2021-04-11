479453e35a2316f6448b0367a948f2a3fa1624ea
Shanto <rakibulhasansanto25@gmail.com>
2021-04-12 00:30:57 +0600

    useState array
    sueState array and remove the single item and also whole item remove button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Setup from './tutorial/1-useState/setup/2-useState-basics'
+//import Setup from './tutorial/1-useState/setup/2-useState-basics'
+import Setup2 from './tutorial/1-useState/setup/3-useState-array'
 function App() {
   return (
     <div className='container'>
       <h2>UseState Example</h2>
-      <Setup />
+      <Setup2 />
     </div>
   )
 }
