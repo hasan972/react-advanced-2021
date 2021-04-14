3cf54f9d2c61702ae7f609f1c8e9b56078bf144f
Shanto <rakibulhasansanto25@gmail.com>
2021-04-14 23:50:58 +0600

    useEffect fdatch-data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 //import Setup from './tutorial/1-useState/setup/2-useState-basics'
-import Setup2 from './tutorial/1-useState/setup/3-useState-array'
+//import Setup2 from './tutorial/1-useState/setup/3-useState-array'
+import Setup2 from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
 function App() {
   return (
     <div className='container'>
