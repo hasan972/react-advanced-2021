02e04fe622d78937a6d571b2b72394fbedbc7992
Shanto <rakibulhasansanto25@gmail.com>
2021-04-21 01:14:33 +0600

    Router Setup

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from 'react'
 //import Setup from './tutorial/1-useState/setup/2-useState-basics'
 //import Setup2 from './tutorial/1-useState/setup/3-useState-array'
 //import Setup2 from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
-import Setup2 from './tutorial/3-conditional-rendering/setup/1-multiple-returns'
-import Setup from './tutorial/4-forms/setup/1-controlled-inputs'
+//import Setup2 from './tutorial/3-conditional-rendering/setup/1-multiple-returns'
+//import Setup from './tutorial/4-forms/setup/1-controlled-inputs'
+import Router from './tutorial/11-react-router/setup'
 function App() {
   return (
     <div className='container'>
-      <h2>UseState Example</h2>
-      <Setup2 />
-      <h2>Controlled-input-from</h2>
-      <Setup />
+      {/* <h2>All about of router</h2> */}
+      <Router />
     </div>
   )
 }
